d05c6cb163a142a8effd60da1ee3f8f44b219f02
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 11:37:03 -0700

    Clarify 'it' statements in specs.

--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -14,7 +14,7 @@ describe('String#word_freq') do
   it('will return 1 if the word occurs once anywhere in the sentence') do
     expect('hello'.word_freq('World hello I am Ian')).to(eq(1))
   end
-  it('will return the number of occurences of the word the method is called on within the sentence') do
+  it('will return the number of occurences of the word the method is called on within a sentence without punctuation') do
     expect('path'.word_freq('Little red riding hood walked down the path and on that path she met me')).to(eq(2))
   end
   it('will return the number of occurences of the word the method is called on  a sentence containing punctuation') do
