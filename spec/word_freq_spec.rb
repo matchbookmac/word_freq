83b32ee2c0bafae4d679f4ee0646c320e56e908d
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 10:59:46 -0700

    Fail spec for finding word in sentence containing the word once anywhere in the sentence.

--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -11,6 +11,9 @@ describe('String#word_freq') do
   it('will return 1 if the word occurs once at the beginning of the sentence') do
     expect('hello'.word_freq('Hello world')).to(eq(1))
   end
+  it('will return 1 if the word occurs once anywhere in the sentence') do
+    expect('hello'.word_freq('World hello I am Ian')).to(eq(1))
+  end
   # it('will return the number of occurences of the word the method is called on within the sentence') do
   #   expect('path'.word_freq('Little red riding hood walked down the path. On that path she met a ...')).to(eq(2))
   # end
