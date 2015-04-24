5af3a99c9d2885c46f4ac07385c0a5c843e10b18
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 10:34:37 -0700

    Pass spec for evaluating if the word and the same word that is capitalized and passed as an argument are equivalent.

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -1,6 +1,7 @@
 class String
   define_method(:word_freq) do |sentence|
-    word = self
-    word.eql?(sentence)
+    word = self.downcase()
+    downcase_sentence = sentence.downcase()
+    word.eql?(downcase_sentence)
   end
 end
