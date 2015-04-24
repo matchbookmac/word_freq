d366bbde5d2ca6c5cb9be68b500e8a3c8fbecb7b
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 10:41:31 -0700

    Pass spec for returning 1 when the word and the sentence passed as an argument are the same word.

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -2,6 +2,9 @@ class String
   define_method(:word_freq) do |sentence|
     word = self.downcase()
     downcase_sentence = sentence.downcase()
-    word.eql?(downcase_sentence)
+    word_count = 0
+    if(word.eql?(downcase_sentence))
+      word_count += 1
+    end
   end
 end
