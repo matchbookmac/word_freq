99ce991dbe0421e53b0e7d2b777ccc57d2de0314
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 11:28:35 -0700

    Fail spec for not finding word in a sentence not containing the word, but contains punctuation.

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -3,7 +3,7 @@ require('pry')
 class String
   define_method(:word_freq) do |sentence|
     search_word = self.downcase()
-    downcase_sentence = sentence.downcase().gsub(/[^a-z0-9\s]/, '').split(/ /)
+    downcase_sentence = sentence.downcase().gsub(/[^a-z0-9\s]/, ' ').split(/ /)
     word_count = 0
     downcase_sentence.each() do |word|
       if(search_word.eql?(word))
