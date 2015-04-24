b27e028d03da7ce294da5970c6319b292013544d
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 10:42:43 -0700

    Fail spec for returning 1 when the word and the sentence passed as an argument are not the same word.

--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -5,9 +5,9 @@ describe('String#word_freq') do
   it('will return 1 if the word the method is called on is the same as the word given as the argument') do
     expect('path'.word_freq('path')).to(eq(1))
   end
-  # it('will return false if the word the method is called on is not the same as the word given as the argument') do
-  #   expect('path'.word_freq('street')).to(eq(false))
-  # end
+  it('will return 0 if the word the method is called on is not the same as the word given as the argument') do
+    expect('path'.word_freq('street')).to(eq(0))
+  end
   # it('will return true if the word the method is called on is the same as the word given as the argument that is capitalized') do
   #   expect('path'.word_freq('Path')).to(eq(true))
   # end
