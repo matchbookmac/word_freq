7daa7c3cdc5ce1ce78d2312ab0ec9aee841ab64b
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 12:17:51 -0700

    Passed spec for finding word in sentence containing word inside of a contraction and punctuation.

--- lib/word_freq.rb
+++ lib/word_freq.rb
@@ -3,10 +3,25 @@ require('pry')
 class String
   define_method(:word_freq) do |sentence|
     search_word = self.downcase()
-    downcase_sentence = sentence.downcase().gsub(/[^a-z0-9\s]/, ' ').split(/ /)
+    if(search_word.gsub(/[^a-z0-9\s]/, '*').include?('*'))
+      downcase_sentence = sentence.downcase().split(/ /)
+      downcase_sentence.map!() do |word|
+        word = word.gsub(/[^a-z0-9\s]/, '*').split(//).reverse()
+        if(word[0].eql?('*'))
+          word.shift()
+        end
+        word = word.reverse().join()
+      end
+    else
+      downcase_sentence = sentence.downcase().gsub(/[^a-z0-9\s]/, ' ').split(/ /)
+    end
     word_count = 0
     downcase_sentence.each() do |word|
-      if(search_word.eql?(word))
+      if(search_word.gsub(/[^a-z0-9\s]/, '*').include?('*'))
+        if(search_word.gsub(/[^a-z0-9\s]/, '*').eql?(word))
+          word_count += 1
+        end
+      elsif(search_word.eql?(word))
         word_count += 1
       end
     end
