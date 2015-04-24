a3bc989b7e6d262a5b6866eb5250c8b045b60bb9
Ian on MacBook Pro <imacd20@gmail.com>
2015-04-24 13:05:19 -0700

    Fail integration test for dynamically changing time to times based on the number of occurrences.

--- spec/word_freq_integration_spec.rb
+++ spec/word_freq_integration_spec.rb
@@ -4,18 +4,18 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('The word_freq path', {:type => :feature}) do
-  it('processes the user\'s word and sentence and returns "1 times" if the word occurs once') do
+  it('processes the user\'s word and sentence and returns "1 time" if the word occurs once') do
     visit('/')
     fill_in('word', :with => 'won\'t')
     fill_in('sentence', :with => 'Won\'t you ask the camel to stop spitting? He is getting spit on my wontons.')
     click_button('Search')
-    expect(page).to have_content('Your word, "won\'t", was found 1 times')
+    expect(page).to have_content('Your word, "won\'t", was found 1 time in')
   end
-  # it('processes the user\'s word and sentence and returns the occurences of the word') do
-  #   visit('/')
-  #   fill_in('word', :with => 'won\'t')
-  #   fill_in('sentence', :with => 'The camel won\'t stop spitting. Won\'t you ask him to stop? Because of his spitting, the dodgers won.')
-  #   click_button('Search')
-  #   expect(page).to have_content('Your word, "won\'t", was found 2 times')
-  # end
+  it('processes the user\'s word and sentence and returns the occurences of the word') do
+    visit('/')
+    fill_in('word', :with => 'won\'t')
+    fill_in('sentence', :with => 'The camel won\'t stop spitting. Won\'t you ask him to stop? Because of his spitting, the dodgers won.')
+    click_button('Search')
+    expect(page).to have_content('Your word, "won\'t", was found 2 times in')
+  end
 end
